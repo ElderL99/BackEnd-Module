7ec7ce1311fbd4b171dda49d526cb53b47563ce2
ElderL99 <adan.lugo.barrientos@outlook.com>
2024-08-19 18:10:25 -0600

    dir usecases with functions post and userscases

---
+++ src/usecases/users.usecases.js
@@ -0,0 +1,61 @@
+const User = require("../models/user.model");
+const encryption = require("../lib/encryption");
+const jwt = require("../lib/jwt");
+const createError = require("http-errors");
+
+async function signUp(data) {
+  const existingUser = await User.findOne({ email: data.email });
+  if (existingUser) {
+    throw createError(409, "Email already in use");
+  }
+
+  if (!data.password) {
+    throw createError(400, "Password is required");
+  }
+
+  const passwordHash = await encryption.encrypt(data.password);
+  data.password = passwordHash;
+
+  const newUser = await User.create(data);
+  return newUser;
+}
+
+async function create(data) {
+  const existingUser = await User.findOne({ email: data.email });
+  if (existingUser) {
+    throw createError(409, "Email already in use");
+  }
+  const newUser = await User.create(data);
+  return newUser;
+}
+
+async function login(data) {
+  const user = await User.findOne({ email: data.email }).select("+password");
+  if (!user) {
+    throw createError(401, "Invalid email or password");
+  }
+
+  const isPasswordValid = await encryption.compare(data.password, user.password);
+  if (!isPasswordValid) {
+    throw createError(401, "Invalid email or password");
+  }
+
+  const token = jwt.sign({ id: user._id });
+  return token;
+}
+
+async function getById(id) {
+  const user = await User.findById(id);
+  return user;
+}
+
+async function deleteById(id) {
+  const user = await User.findById(id);
+  if (!user) {
+    throw createError(404, "User not found");
+  }
+  const deletedUser = await User.findByIdAndDelete(id);
+  return deletedUser;
+}
+
+module.exports = { create, login, getById, deleteById, signUp };
